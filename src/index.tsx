a8618c0201a9fb193c987d8706f5188585e45bf6
Hussain Ruhullah <hussain.ruhollah@gmail.com>
2019-04-05 15:02:40 +0200

    Provider
    Providing Store To Provider

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,23 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import { createStore } from 'redux';
+import { enthusiasm } from './reducers/index';
+import { StoreState } from './types/index';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { EnthusiasmAction } from './actions/index';
+
+const store = createStore<StoreState, EnthusiasmAction, any, any>(enthusiasm, {
+  enthusiasmLevel: 1,
+  languageName: 'TypeScript',
+});
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement
+  <Provider store={ store }>
+      <App />
+  </Provider>,
+    document.getElementById('root') as HTMLElement
+
 );
 registerServiceWorker();
